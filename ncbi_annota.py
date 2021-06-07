477fe7b9155238a067f43df1310255348dd786f3
Eric Dou <bj600800@gmail.com>
2021-06-07 17:33:05 +0800

    Delete ncbi_annota.py

--- ncbi_annota.py
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env python           
-# -*- coding:utf-8 -*-          
-# @Filename:    ncbi_annota.py      
-# @Author:      Eric Dou        
-# @Time:        2021/5/17 9:37 
-
-'''Function: '''
-
