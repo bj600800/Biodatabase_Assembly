536fca91f5d29ce7e73052fdd0bd7f57ac1cd0b8
Eric Dou <bj600800@gmail.com>
2021-06-07 17:32:33 +0800

    Enzyme region annotation by Genbank flatfiles.

---
+++ ncbi_annota.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python           
+# -*- coding:utf-8 -*-          
+# @Filename:    ncbi_annota.py      
+# @Author:      Eric Dou        
+# @Time:        2021/5/17 9:37 
+
+'''Function: '''
+
